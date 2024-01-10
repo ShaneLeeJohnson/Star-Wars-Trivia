dca7c36bdd741aa98d32e2d8f23134952a9e9fed
DannySanchez03 <dannyproa303@gmail.com>
2024-01-09 21:07:00 -0600

    Added the id's for the script so that elements can be used for dom manipulation

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+highScoreBtnDiv = document.getElementById('highScoreDivBtn');
+startingInfoDiv = document.getElementById('startingInfoDiv');
+startGameBtn = document.getElementById('startGameBtn');
+potentialQuestionDiv = document.getElementById('potentialQuestionDiv');
+question = document.getElementById('question');
+answerForm = document.getElementById('answerForm');
